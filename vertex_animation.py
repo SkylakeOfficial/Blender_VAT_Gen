eefb2605102e5f97fa3dd567f12b6a07f070b7de
SkylakeOfficial <60763730+SkylakeOfficial@users.noreply.github.com>
2023-11-07 17:57:40 +0800

    Deleted my Rigid stuff, that's stupid, I'm an idiot.

--- vertex_animation.py
+++ vertex_animation.py
@@ -37,11 +37,6 @@ import bmesh
 
 
 class RigidSettings(bpy.types.PropertyGroup):
-    detect_rigid: bpy.props.BoolProperty(
-        name="DetectRigidVertexGroups",
-        description="If you have vertices that share the same transform like rigid stuff, assign them to vertex groups with 'RGD' prefixes. Verts under the same group would share transforms, reducing the offset texture size. Enabling this disables the Normal output ",
-        default=False
-    )
     reference_frame: bpy.props.IntProperty(
         name="ReferenceFrame",
         description="The reference frame of mesh",
@@ -49,11 +44,9 @@ class RigidSettings(bpy.types.PropertyGroup):
     )
 
 
-def get_per_frame_mesh_data(context, data, objects, ref_frame, rigid, rigid_groups):
+def get_per_frame_mesh_data(context, data, objects, ref_frame):
     """Return a list of combined mesh data per frame"""
     meshes = []
-    rigid_vert_groups = dict()
-
     for i in frame_range(context.scene):
 
         context.scene.frame_set(i)
@@ -70,69 +63,41 @@ def get_per_frame_mesh_data(context, data, objects, ref_frame, rigid, rigid_grou
         bm.free()
         me.calc_normals()
         meshes.append(me)
-
-        if rigid and i == ref_frame:
-            for g in rigid_groups:
-                rigid_vert_groups[g] = []
-            vert_count = 0
-            for ob in objects:
-                group_lookup = {gl.index: gl.name for gl in ob.vertex_groups}
-                for v in ob.data.vertices:
-                    for g in v.groups:
-                        if group_lookup[g.group] in rigid_groups:
-                            rigid_vert_groups[group_lookup[g.group]].append(v.index+vert_count)
-                            break
-                vert_count += len(ob.data.vertices)
-
         if i == ref_frame:
             export_mesh = me.copy()
 
     meshes.remove(meshes[len(meshes)-1])
-    return meshes, export_mesh, rigid_vert_groups
+    return meshes, export_mesh
 
 
-def create_export_mesh_object(context, data, me, rigid, rigid_vert_groups):
+def create_export_mesh_object(context, data, me):
     """Return a mesh object with correct UVs"""
     while len(me.uv_layers) < 2:
         me.uv_layers.new()
     uv_layer = me.uv_layers[1]
     uv_layer.name = "vertex_anim"
-    if rigid:
-        num_groups = len(rigid_vert_groups)
-        for index, i in enumerate(rigid_vert_groups.values()):
-            for vi in i:
-                uv_layer.data[vi].uv = (
-                    (index + 0.5)/num_groups, 128/255
-                )
-    else:
-        for loop in me.loops:
-            uv_layer.data[loop.index].uv = (
-                (loop.vertex_index + 0.5)/len(me.vertices), 128/255
-            )
+
+    for loop in me.loops:
+        uv_layer.data[loop.index].uv = (
+            (loop.vertex_index + 0.5)/len(me.vertices), 128/255
+        )
     ob = data.objects.new("export_mesh", me)
     context.scene.collection.objects.link(ob)
     return ob
 
 
-def get_vertex_data(data, meshes, refmesh, rigid, rigid_vert_groups):
+def get_vertex_data(data, meshes, refmesh):
     """Return lists of vertex offsets and normals from a list of mesh data"""
     offsets = []
     normals = []
     for me in reversed(meshes):
-        if rigid:
-            for i in rigid_vert_groups.values():
-                vi = i[0]
-                offset = (me.vertices[vi].co - refmesh.vertices[vi].co)*100.0
-                x, y, z = offset
-                offsets.extend((x, -y, z, 1))
-        else:
-            for v in me.vertices:
-                # Use the default scene scale instead since the send2UE plugin will fix that.
-                offset = (v.co - refmesh.vertices[v.index].co)*100.0
-                x, y, z = offset
-                offsets.extend((x, -y, z, 1))
-                x, y, z = v.normal
-                normals.extend(((x + 1) * 0.5, (-y + 1) * 0.5, (z + 1) * 0.5, 1))
+        for v in me.vertices:
+            # Use the default scene scale instead since the send2UE plugin will fix that.
+            offset = (v.co - refmesh.vertices[v.index].co)*100.0
+            x, y, z = offset
+            offsets.extend((x, -y, z, 1))
+            x, y, z = v.normal
+            normals.extend(((x + 1) * 0.5, (-y + 1) * 0.5, (z + 1) * 0.5, 1))
         if not me.users:
             data.meshes.remove(me)
     return offsets, normals
@@ -143,7 +108,7 @@ def frame_range(scene):
     return range(scene.frame_start, scene.frame_end + scene.frame_step, scene.frame_step)
 
 
-def bake_vertex_data(context, data, offsets, normals, size, rigid):
+def bake_vertex_data(context, data, offsets, normals, size):
     """Stores vertex offsets and normals in separate image textures"""
     width, height = size
     offset_texture = data.images.new(
@@ -154,14 +119,13 @@ def bake_vertex_data(context, data, offsets, normals, size, rigid):
         float_buffer=True
     )
     offset_texture.pixels = offsets
-    if not rigid:
-        normal_texture = data.images.new(
-            name="normals",
-            width=width,
-            height=height,
-            alpha=True
-        )
-        normal_texture.pixels = normals
+    normal_texture = data.images.new(
+        name="normals",
+        width=width,
+        height=height,
+        alpha=True
+    )
+    normal_texture.pixels = normals
 
 
 class OBJECT_OT_ProcessAnimMeshes(bpy.types.Operator):
@@ -187,7 +151,6 @@ class OBJECT_OT_ProcessAnimMeshes(bpy.types.Operator):
         return ob and ob.type == 'MESH' and ob.mode == 'OBJECT'
 
     def execute(self, context):
-        rigid = context.scene.rigid_settings.detect_rigid
         ref_frame = context.scene.rigid_settings.reference_frame
         units = context.scene.unit_settings
         data = bpy.data
@@ -213,7 +176,7 @@ class OBJECT_OT_ProcessAnimMeshes(bpy.types.Operator):
         #      "Scene Unit must be Metric with a Unit Scale of 0.01!"
         #   )
         #return {'CANCELLED'}
-        if vertex_count > 8192 and not rigid:
+        if vertex_count > 8192:
             self.report(
                 {'ERROR'},
                 f"Vertex count of {vertex_count :,}, execedes limit of 8,192!"
@@ -225,32 +188,12 @@ class OBJECT_OT_ProcessAnimMeshes(bpy.types.Operator):
                 f"Frame count of {frame_count :,}, execedes limit of 8,192!"
             )
             return {'CANCELLED'}
-        rigid_group_list = []
-        if rigid:
-            for ob in objects:
-                for g in ob.vertex_groups:
-                    if g.name.startswith('RGD'):
-                        if g.name not in rigid_group_list:
-                            rigid_group_list.append(g.name)
-            if len(rigid_group_list) == 0:
-                self.report(
-                    {'ERROR'},
-                    "No RGD vertex group detected!"
-                )
-                return {'CANCELLED'}
-            if len(rigid_group_list) > 8192:
-                self.report(
-                    {'ERROR'},
-                    "Are you kidding me you have too many RGD groups!"
-                )
-                return {'CANCELLED'}
-            vertex_count = len(rigid_group_list)
 
-        meshes, export_mesh_data, rigid_vert_groups = get_per_frame_mesh_data(context, data, objects, ref_frame, rigid, rigid_group_list)
-        create_export_mesh_object(context, data, export_mesh_data, rigid, rigid_vert_groups)
-        offsets, normals = get_vertex_data(data, meshes, export_mesh_data, rigid, rigid_vert_groups)
+        meshes, export_mesh_data = get_per_frame_mesh_data(context, data, objects, ref_frame)
+        create_export_mesh_object(context, data, export_mesh_data)
+        offsets, normals = get_vertex_data(data, meshes, export_mesh_data)
         texture_size = vertex_count, frame_count
-        bake_vertex_data(context, data, offsets, normals, texture_size, rigid)
+        bake_vertex_data(context, data, offsets, normals, texture_size)
         return {'FINISHED'}
 
 
@@ -270,7 +213,6 @@ class VIEW3D_PT_VertexAnimation(bpy.types.Panel):
         layout.prop(scene, "frame_end", text="End")
         layout.prop(scene, "frame_step", text="Step")
         rigid_prop = context.scene.rigid_settings
-        layout.prop(rigid_prop, "detect_rigid", text="Detect Rigid Groups")
         layout.prop(rigid_prop, "reference_frame", text="ReferenceFrame")
         row1 = layout.row()
         row1.operator("object.process_anim_meshes")
